36faa314033b588dad3083f5ff1268d2ed788dc5
Damian526 <damianzieba999@gmail.com>
2024-07-23 10:26:05 +0200

    Rate limiting

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const productRouter = require('./routes/productRoutes');
@@ -11,6 +12,13 @@ const cors = require('cors');
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many reuests from this IP, please try again in an hour',
+});
+app.use('/api', limiter);
 app.use(express.json());
 app.use(cors());
 app.use(express.static(`${__dirname}/public`));
