f9059e07ae7e2eca5d40283841b9218638898d9e
Damian526 <damianzieba999@gmail.com>
2024-07-23 11:21:08 +0200

    Data sanatization

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const productRouter = require('./routes/productRoutes');
@@ -27,7 +29,15 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 // body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+
 app.use(cors());
+
 // app.use(express.static(`${__dirname}/public`));
 
 // 3) ROUTES
