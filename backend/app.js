e42e8e08de069b29071a5c41c0624c56e6389537
Damian526 <damianzieba999@gmail.com>
2024-07-23 11:12:33 +0200

    Setting security hTTP headers

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const productRouter = require('./routes/productRoutes');
@@ -8,7 +9,11 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 const cors = require('cors');
 
-// 1) MIDDLEWARES
+// 1)Global MIDDLEWARES
+
+//Set security HTTP headers
+app.use(helmet());
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
@@ -18,10 +23,12 @@ const limiter = rateLimit({
   windowMs: 60 * 60 * 1000,
   message: 'Too many reuests from this IP, please try again in an hour',
 });
+
 app.use('/api', limiter);
-app.use(express.json());
+// body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
 app.use(cors());
-app.use(express.static(`${__dirname}/public`));
+// app.use(express.static(`${__dirname}/public`));
 
 // 3) ROUTES
 app.use('/api/v1/products', productRouter); // ends code here if there is correct route
