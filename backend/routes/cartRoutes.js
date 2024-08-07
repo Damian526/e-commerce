8790236d54eb131294ab5356a315cd6774e189ef
Damian526 <damianzieba999@gmail.com>
2024-08-07 22:47:17 +0200

    Fix incorrect import path for cartController

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cartController = require('../controllers/CartController');
+const cartController = require('../controllers/cartController');
 const authController = require('../controllers/authController');
 
 const router = express.Router();
